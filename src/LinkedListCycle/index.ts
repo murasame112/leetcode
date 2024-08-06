bf58bb01a7d2ee7da33e19f6511f9be9f81beb12
murasame112 <tomaszwiesek00@gmail.com>
2024-08-06 13:52:26 +0200

    linked list cycle done

--- src/LinkedListCycle/index.ts
+++ src/LinkedListCycle/index.ts
@@ -7,6 +7,66 @@ class ListNode {
   }
 }
 
+
 function hasCycle(head: ListNode | null): boolean {
-  return true;
+	if(head == null || head.next == null){
+		return false;
+	}
+
+	let fast: ListNode | null = head.next.next;
+	let slow: ListNode | null = head.next;
+
+	while(1){
+		if(fast == null || slow == null || fast.next == null){
+			return false;
+		}
+		if(fast == slow){
+			return true;
+		}
+		fast = fast.next.next;
+		slow = slow.next;
+
+	}
+
+	return false;
 }
+
+
+// === my answer: 
+// const listIndexes: Record<number, ListNode | null> = {}
+// function hasCycle(head: ListNode | null): boolean {
+
+// 	if(head == null || head.next == null){
+// 		return false;
+// 	}
+
+// 	if(head.val in listIndexes){
+// 		return true;
+// 	}else{
+// 		listIndexes[head.val] = head.next;
+// 		if(head.next == null){
+// 			return false;
+// 		}
+// 		return hasCycle(head.next);
+// 	}
+// }
+
+
+let head = [3,2,0,-4];
+let pos = 1;
+
+// let head = [1,2];
+// let pos = -1;
+
+
+let h3 = new ListNode(head[3]);
+let h2 = new ListNode(head[2], h3);
+let h1 = new ListNode(head[1], h2);
+let h0 = new ListNode(head[0], h1);
+
+h3.next = h1;
+
+// let h1 = new ListNode(head[1]);
+// let h0 = new ListNode(head[0], h1);
+
+console.log(hasCycle(h0));
