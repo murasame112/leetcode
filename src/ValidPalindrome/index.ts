82aec7ebb97bd9dec9209efecfadbf8ecb91a2f6
murasame112 <tomaszwiesek00@gmail.com>
2024-08-12 11:22:23 +0200

    valid palindrome done

--- src/ValidPalindrome/index.ts
+++ src/ValidPalindrome/index.ts
@@ -1,9 +1,25 @@
 function isPalindrome(s: string): boolean {
-  
+  s = s.replace(/[^a-zA-Z0-9]/g, '');
+	s = s.toLocaleLowerCase();
+
+	if(s.length == 0){
+		return true;
+	}
+	let left = 0;
+	let right = s.length-1;
+
+	while(left < right){
+		if(s[left] != s[right]){
+			return false;
+		}
+		left++;
+		right--;
+	}
+
 	return true;
 };
 
 
 
-const s = "A man, a plan, a canal: Panama";
+const s = "aa";
 console.log(isPalindrome(s));
