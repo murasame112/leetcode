7a805aaa8558f073f29ba9a09477ef1c452ca29a
murasame112 <tomaszwiesek00@gmail.com>
2024-08-29 12:30:34 +0200

    two sums II done

--- src/TwoSumII/index.ts
+++ src/TwoSumII/index.ts
@@ -1,5 +1,15 @@
 function twoSum(numbers: number[], target: number): number[] {
-  
+	let left = 0;
+	let right = numbers.length - 1;
+	while (1){
+		if ((numbers[left] + numbers[right]) > target){
+			right--;
+		}else if ((numbers[left] + numbers[right]) < target){
+			left++;
+		}else {
+			return [left + 1, right + 1];
+		}
+	}
 	return [];
 };
 
