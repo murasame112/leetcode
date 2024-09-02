b33006dcb632fa4a2163bd190b955b3d9589555f
murasame112 <tomaszwiesek00@gmail.com>
2024-09-02 12:02:45 +0200

    Compare Version Numbers setup

---
+++ src/CompareVersionNumbers/index.ts
@@ -0,0 +1,9 @@
+function compareVersion(version1: string, version2: string): number {
+    
+	return 0;
+};
+
+let version1 = '1.2';
+let version2 = '1.10';
+
+console.log(compareVersion(version1, version2));
