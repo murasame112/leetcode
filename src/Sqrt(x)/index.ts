6fc85a46655a8429a2518d4c532205de07335f39
murasame112 <tomaszwiesek00@gmail.com>
2024-09-08 16:09:38 +0200

    sqrt(x) done

--- src/Sqrt(x)/index.ts
+++ src/Sqrt(x)/index.ts
@@ -1,6 +1,29 @@
+// binary search practice
 function mySqrt(x: number): number {
-  return x;
+	if (x == 0){
+		return 0;
+	}
+	if (x == 1){
+		return 1;
+	}
+	let min: number = 0;
+	let max: number = x;
+	let mid: number = Math.floor(min + ((max - min)/2));
+	let result = mid * mid;
+
+	while (result != x && mid != min){
+		if (result < x){
+			min = mid;
+			mid = Math.floor(min + ((max - min)/2));
+		} else if (result > x){
+			max = mid
+			mid = Math.floor(min + ((max - min)/2));
+		}
+		result = mid * mid;
+	}
+
+  return mid;
 };
 
-const x = 4;
+const x = 41;
 console.log(mySqrt(x));
