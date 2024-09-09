b2340de0569e1da94846680dc567741b1d68b9ba
murasame112 <tomaszwiesek00@gmail.com>
2024-09-09 14:50:03 +0200

    distance value setup

---
+++ src/DistanceValueBetweenTwoArrays/index.ts
@@ -0,0 +1,9 @@
+function findTheDistanceValue(arr1: number[], arr2: number[], d: number): number {
+    
+	return d;
+};
+
+const arr1 = [4,5,8];
+const arr2 = [10,9,1,8];
+const d = 2;
+console.log(findTheDistanceValue(arr1, arr2, d));
