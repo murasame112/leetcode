b953780153d82d946e4ea9370c660df706b328bd
murasame112 <tomaszwiesek00@gmail.com>
2024-09-09 13:14:38 +0200

    guess number setup

---
+++ src/GuessNumber/index.ts
@@ -0,0 +1,18 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+
+ function guessNumber(n: number): number {
+
+	return n;
+
+ };
+
+ const n = 5;
+ console.log(guessNumber(n));
