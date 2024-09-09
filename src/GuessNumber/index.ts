e2a73ded9cb0a9659ece8a2599675d1b181998e9
murasame112 <tomaszwiesek00@gmail.com>
2024-09-09 13:37:19 +0200

    guess number done

--- src/GuessNumber/index.ts
+++ src/GuessNumber/index.ts
@@ -9,10 +9,36 @@
 
 
  function guessNumber(n: number): number {
+	// function from api, here was my example implementation used for testing
+ 	// let guess = function(num: number): number {
+	// 	if(num == 6){
+	// 		return 0;
+	// 	}else if( num > 6){
+	// 		return 1;
+	// 	}else if (num < 6){
+	// 		return -1;
+	// 	}
+	// 	return 0;
+	// }
 
-	return n;
+	let min = 1;
+	let max = n;
+	let mid = Math.floor(min + (max - min)/2);
+	while (1){
+
+		if (guess(mid) === 0){
+			return mid;
+		}else if (guess(mid) === 1){
+			max = mid-1;
+			mid = Math.floor(min + (max - min)/2);
+		}else if (guess(mid) === -1){
+			min = mid+1;
+			mid = Math.floor(min + (max - min)/2);
+		}
+	}
+	return mid;
 
  };
 
- const n = 5;
+ const n = 10;
  console.log(guessNumber(n));
