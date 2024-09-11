f4a189ef56c8df3222ad7712bf5346f92b3ad57b
murasame112 <tomaszwiesek00@gmail.com>
2024-09-11 19:43:32 +0200

    find min done

--- src/FindMin/index.ts
+++ src/FindMin/index.ts
@@ -1,8 +1,35 @@
 function findMin(nums: number[]): number {
-    
+    let min = 0;
+    let max = nums.length - 1;
+    let mid = 0;
 
-	return 0;
-};
+    while (min <= max) {
+        mid = Math.floor(min + (max - min) / 2);
 
-const nums = [3,4,5,1,2];
-console.log(findMin(nums));
+        if (nums[max] > nums[min]) {
+            return nums[min];
+        }
+        if (nums[mid] > nums[min]) {
+            min = mid;
+        } else if (nums[mid] < nums[min]) {
+            max = mid;
+        } else {
+            return Math.min(nums[min], nums[max]);
+        }
+    }
+
+    return mid;
+}
+
+const nums = [5, 1, 2, 3, 4];
+const nums2 = [2, 4, 5, 6, 7, 0, 1];
+const nums3 = [11, 13, 15, 17];
+console.log(findMin(nums3));
+
+/*
+			console.log('nums: ',nums);
+			console.log('min: ', min);
+			console.log('max: ', max);
+			console.log('mid: ', mid);
+			console.log('=============');
+*/
