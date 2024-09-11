8e6c3657b9986cdac352907a60ea799e2482f924
murasame112 <tomaszwiesek00@gmail.com>
2024-09-11 13:28:23 +0200

    arranging coins done

--- src/ArrangingCoins/index.ts
+++ src/ArrangingCoins/index.ts
@@ -1,7 +1,38 @@
+// binary search solution
 function arrangeCoins(n: number): number {
-    
-	return n;
-};
+    let min = 1;
+    let max = n;
 
+    while (min <= max) {
+        let mid = Math.floor(min + (max - min) / 2);
+        let sum = (mid * (mid + 1)) / 2; // gauss summation
+        if (sum === n) {
+            return mid;
+        } else if (sum > n) {
+            max = mid - 1;
+        } else {
+            min = mid + 1;
+        }
+    }
+
+    return max;
+}
 const n = 5;
 console.log(arrangeCoins(n));
+
+// first solution, based on intuition
+/*
+function arrangeCoins(n: number): number {
+    let i = 1;
+		let sum = 0;
+		while (sum < n){
+			
+			i++;
+			sum += i;
+		}
+		i--;
+		return i
+
+};
+
+*/
