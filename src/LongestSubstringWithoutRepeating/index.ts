65dd8f3210047d7e5231fdfe4f0294c38d8de775
murasame112 <tomaszwiesek00@gmail.com>
2024-09-12 19:28:36 +0200

    Longest substring without repeating setup

---
+++ src/LongestSubstringWithoutRepeating/index.ts
@@ -0,0 +1,9 @@
+function lengthOfLongestSubstring(s: string): number {
+    
+	return 0;
+};
+
+// link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+const s = 'abcabcbb';
+console.log(lengthOfLongestSubstring(s));
