ad4652db8b43e2b9152aa71694be8a930ae430e0
murasame112 <tomaszwiesek00@gmail.com>
2024-09-13 22:25:55 +0200

    second solution fo best time to buy

--- src/BestTimeToBuy/index.ts
+++ src/BestTimeToBuy/index.ts
@@ -3,25 +3,43 @@ function maxProfit(prices: number[]): number {
 	let right = 1;
 	let sum = 0;
 	while (left < prices.length){
-		if (prices[right] - prices[left] > sum){
-			sum = prices[right] - prices[left];
+
+		if (prices[right] > prices[left]){
+			sum = Math.max(sum, prices[right] - prices[left]);
+		}else{
+			left = right;
 		}
 		right++;
-		if (right >= prices.length){
-			left++;
-			right = left + 1;
-		}
-
 	}
     return sum;
 };
 
-//const prices = [7,1,5,3,6,4];
-const prices = [7,6,4,3,1];
+// 2 - 6
+const prices = [7,8,2,5,4,6,1,3];
+//const prices = [7, 6, 4, 3, 1];
 console.log(maxProfit(prices));
 
+// second solution, faster, also with two pointers
+/*
+function maxProfit(prices: number[]): number {
+	let left = 0;
+	let right = 1;
+	let sum = 0;
+	while (left < prices.length){
 
-// first solution (brute force);
+		if (prices[right] > prices[left]){
+			sum = Math.max(sum, prices[right] - prices[left]);
+		}else{
+			left = right;
+		}
+		right++;
+	}
+    return sum;
+};
+
+*/
+
+// first solution (brute force, time limit exceeded);
 
 /*
 function maxProfit(prices: number[]): number {
