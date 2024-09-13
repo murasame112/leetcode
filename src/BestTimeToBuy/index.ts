2771d5fa4b3591f08d885748154001347136836a
murasame112 <tomaszwiesek00@gmail.com>
2024-09-13 21:55:08 +0200

    best time to buy first solution

--- src/BestTimeToBuy/index.ts
+++ src/BestTimeToBuy/index.ts
@@ -1,6 +1,45 @@
 function maxProfit(prices: number[]): number {
-    return 0;
+	let left = 0;
+	let right = 1;
+	let sum = 0;
+	while (left < prices.length){
+		if (prices[right] - prices[left] > sum){
+			sum = prices[right] - prices[left];
+		}
+		right++;
+		if (right >= prices.length){
+			left++;
+			right = left + 1;
+		}
+
+	}
+    return sum;
 };
 
-const prices = [7,1,5,3,6,4];
+//const prices = [7,1,5,3,6,4];
+const prices = [7,6,4,3,1];
 console.log(maxProfit(prices));
+
+
+// first solution (brute force);
+
+/*
+function maxProfit(prices: number[]): number {
+	let left = 0;
+	let right = 1;
+	let sum = 0;
+	while (left < prices.length){
+		if (prices[right] - prices[left] > sum){
+			sum = prices[right] - prices[left];
+		}
+		right++;
+		if (right >= prices.length){
+			left++;
+			right = left + 1;
+		}
+
+	}
+    return sum;
+};
+
+*/
