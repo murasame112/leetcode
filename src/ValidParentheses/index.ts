0e753ef1b8d7826c69fe6df4c0304b2c846d94fb
murasame112 <tomaszwiesek00@gmail.com>
2024-09-23 09:29:26 +0200

    valid parentheses done

--- src/ValidParentheses/index.ts
+++ src/ValidParentheses/index.ts
@@ -1,10 +1,52 @@
 function isValid(s: string): boolean {
+    const startStack: string[] = s.split('');
+    const finalStack: string[] = [];
 
-	return true;
-};
+    if (killOdd(s)) {
+        return false;
+    }
 
+    let length = startStack.length;
+    for (let i = 0; i <= length; i++) {
+        if (
+            checkBrackets(
+                startStack[startStack.length - 1],
+                finalStack[finalStack.length - 1]
+            )
+        ) {
+            startStack.pop();
+            finalStack.pop();
+        } else {
+            if (startStack.length > 0) {
+                let bracket = startStack.pop();
+                finalStack.push(bracket!);
+            } else {
+                if (finalStack.length > 0) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+    }
 
-const s = "()";
+    return false;
+}
+
+function checkBrackets(a: string, b: string) {
+    if (a === '(' && b === ')') return true;
+    if (a === '[' && b === ']') return true;
+    if (a === '{' && b === '}') return true;
+    return false;
+}
+
+function killOdd(a: string) {
+    if (a.length % 2 === 0) {
+        return false;
+    }
+    return true;
+}
+
+//const s = "()[]{})";
+const s = '([]{})';
 console.log(isValid(s));
-
-
