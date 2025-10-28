c502662c5de44a34020dc25e4b57954aece004f4
murasame112 <tomaszwiesek00@gmail.com>
2025-10-28 23:01:52 +0100

    container with most water done

--- src/ContainerWithMostWater/index.ts
+++ src/ContainerWithMostWater/index.ts
@@ -1,7 +1,20 @@
 function maxArea(height: number[]): number {
-    return 0;
+	let a = 0, b = height.length - 1, highest = 0, res = 0;
+
+	while(a <= b){
+		if(height[a] <= height[b]){
+			res = height[a] * (b - a);
+			a++;
+		}else{
+			res = height[b] * (b - a);
+			b--;
+		}
+		if(res > highest) highest = res;
+	}
+  return highest;
 };
 
-const height = [1,8,6,2,5,4,8,3,7];
+const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+//const height = [1, 8, 6, 2, 7];
 
 console.log(maxArea(height));
