f8a9acb41ed82753ceba296ffb423eeb8216d079
murasame112 <tomaszwiesek00@gmail.com>
2025-10-28 18:05:24 +0100

    move zeroes

--- src/MoveZeroes/index.ts
+++ src/MoveZeroes/index.ts
@@ -2,5 +2,30 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-    
+	if (nums.length <= 1) return;
+	let a = 0, b = 0;
+	let bufor = 0;
+
+	while(b <= nums.length - 1){
+		if(nums[a] == 0 && nums[b] != 0){
+			bufor = nums[a];
+			nums[a] = nums[b];
+			nums[b] = bufor;
+		} 
+
+		if(nums[a] != 0){
+			a++;
+		}
+		if(nums[b] == 0 || b <= a){
+			b++;
+		}
+	}
+
 };
+
+//const nums = [2,0,1,0,3,12];
+//const nums = [0];
+//const nums = [1];
+//const nums = [2,1];
+const nums = [0,0,0,0,0,2,5]
+console.log(moveZeroes(nums));
