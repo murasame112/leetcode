5f7f9b38adf7af7d43b1b3a4d8e708072bb30838
murasame112 <tomaszwiesek00@gmail.com>
2025-10-30 15:24:40 +0100

    max num of vowels done

--- src/MaxNumofVowels/index.ts
+++ src/MaxNumofVowels/index.ts
@@ -1,6 +1,25 @@
 function maxVowels(s: string, k: number): number {
-	return 0;
+	let vowels: Record<string, number> = {a : 1, e : 1, i : 1, o : 1, u : 1};
+	let a = 0;
+	let counter = 0, max = 0;
+	for (let i = 0; i < k; i++){
+		if(vowels[s[i]]) counter++;
+	}
+	if(counter == k) return counter;
+	max = counter;
+	
+	while (a + k <= s.length){
+		if(vowels[s[a]]) counter--;
+		a++;
+		if(vowels[s[a + k-1]]) counter++;
+		if(counter == k) return counter;
+		if(counter > max) max = counter;
+	}
+
+	return max;
 };
 
-const s = "abciiidef", k = 3;
+//const s = "abciiidef", k = 3;]
+const s = "aeiou", k = 2;
+//const s = "leetcode", k = 3
 console.log(maxVowels(s, k));
