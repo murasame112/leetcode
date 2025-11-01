409d75d6f3f1b15510eacc53f43b962ea3c571d0
murasame112 <tomaszwiesek00@gmail.com>
2025-11-01 21:36:14 +0100

    FindTheHighestAltitude done

--- src/FindTheHighestAltitude/index.ts
+++ src/FindTheHighestAltitude/index.ts
@@ -1,7 +1,16 @@
 function largestAltitude(gain: number[]): number {
-  return 0;  
+	let start = 0, max = start;
+
+	for(let i = 0; i < gain.length; i++){
+		console.log(start);
+		start += gain[i];
+		if(max < start) max = start;
+	}
+
+  return max;
 };
 
-const gain = [-5,1,5,0,-7];
+//const gain = [-5,1,5,0,-7];
+const gain = [-4,-3,-2,-1,4,3,2];
 
 console.log(largestAltitude(gain));
