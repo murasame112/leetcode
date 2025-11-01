774132fc904fe4851986ea12d60545def68549e8
murasame112 <tomaszwiesek00@gmail.com>
2025-11-01 13:39:16 +0100

    LongestSubarrayof1s done

--- src/LongestSubarrayof1s/index.ts
+++ src/LongestSubarrayof1s/index.ts
@@ -1,7 +1,29 @@
 function longestSubarray(nums: number[]): number {
-    return 0;
+	// this is just a specific case of maxConsecutiveOnesIII, so i used the same code ¯\_(ツ)_/¯
+	let a = 0, b = 0, max = 0, counter = 0, zeroes = 0;
+	if(nums[b] == 0) zeroes++;
+	while(b < nums.length && a < nums.length){
+
+		if(nums[b] == 1 || zeroes <= 1){
+			b++;
+			if(nums[b] == 0) zeroes++;
+		}
+		
+		if(zeroes == 2){
+			counter = b - a;
+			if(max < counter) max = counter;
+			if(nums[a] == 0){
+				zeroes--;
+			}
+			a++;
+		}
+	
+	}
+	counter = b - a;
+	if(max < counter) max = counter;
+	return max - 1;
 };
 
 
-const nums = [0,1,1,1,0,1,1,0,1];
+const nums = [1,1,1];
 console.log(longestSubarray(nums));
