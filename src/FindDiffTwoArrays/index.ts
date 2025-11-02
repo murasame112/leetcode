d454f5310b949ba8901b71d0d65b3a48ada3de35
murasame112 <tomaszwiesek00@gmail.com>
2025-11-02 15:21:16 +0100

    FindDiffTwoArrays setup

---
+++ src/FindDiffTwoArrays/index.ts
@@ -0,0 +1,9 @@
+function findDifference(nums1: number[], nums2: number[]): number[][] {
+	
+    
+	return [][0];
+};
+
+
+const nums1 = [1,2,3], nums2 = [2,4,6];
+console.log(findDifference(nums1, nums2));
