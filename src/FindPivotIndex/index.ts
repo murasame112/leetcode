7b2bdfaca96b12ff7316c510be1b1bafaf5bc813
murasame112 <tomaszwiesek00@gmail.com>
2025-11-02 15:11:54 +0100

    find pivot index done

--- src/FindPivotIndex/index.ts
+++ src/FindPivotIndex/index.ts
@@ -1,7 +1,20 @@
 function pivotIndex(nums: number[]): number {
-    return 0;
+	let leftSum = 0, rightSum = 0, pivot = 0 ;
+	for(let i = 0; i < nums.length; i++){
+		rightSum += nums[i];
+	}
+	while(pivot < nums.length){
+		rightSum -= nums[pivot];
+		if(leftSum == rightSum) return pivot;
+		leftSum += nums[pivot];
+		pivot++;
+	}
+
+	return -1;
 };
 
 const nums = [1,7,3,6,5,6];
+//const nums = [1,2,3];
+//const nums = [2, 1, -1]; // powinno byc 0
 
 console.log(pivotIndex(nums));
