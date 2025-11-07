aebc59e834c287afc63ee15cd15c7fe6405adefb
murasame112 <tomaszwiesek00@gmail.com>
2025-11-07 14:47:54 +0100

    Remove Stars done

--- src/RemoveStars/index.ts
+++ src/RemoveStars/index.ts
@@ -1,7 +1,18 @@
 function removeStars(s: string): string {
-    
-	return '';
+	const stack: string[] = [];
+	let pointer = 0;
+	while(pointer < s.length){
+		if(s[pointer] == '*'){
+			stack.pop();
+		}else{
+			stack.push(s[pointer]);
+		}
+		pointer++;
+	}
+
+	return stack.join('');
 };
 
 const s = "leet**cod*e";
+//const s = "erase*****";
 console.log(removeStars(s));
