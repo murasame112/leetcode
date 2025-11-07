8d012ce12bc924e0276f06c5e075dd23b247586c
murasame112 <tomaszwiesek00@gmail.com>
2025-11-07 12:20:17 +0100

    DetermineIfTwoStringsAreClose 2nd solution

--- src/DetermineIfTwoStringsAreClose/index.ts
+++ src/DetermineIfTwoStringsAreClose/index.ts
@@ -1,5 +1,71 @@
 function closeStrings(word1: string, word2: string): boolean {
 	if(word1.length !== word2.length) return false;
+	if(word1 === word2) return true;
+	
+	const map1 = new Map<string, number>();
+	const map2 = new Map<string, number>();
+	let i = 0;
+	
+	while(i < word1.length){
+		if(map1.has(word1[i])){
+			map1.set(word1[i], map1.get(word1[i])! + 1)
+		}else{
+			map1.set(word1[i], 1);
+		}
+		if(map2.has(word2[i])){
+			map2.set(word2[i], map2.get(word2[i])! + 1)
+		}else{
+			map2.set(word2[i], 1);
+		}
+		i++;
+	}
+
+	const arr1: number[] = [], arr2: number[] = [];
+	for(const [key, val] of map1) {
+		if(!(map2.has(key))){
+			return false;
+		} 
+
+		arr1.push(val);
+		arr2.push(map2.get(key)!);
+	};
+	const compareValues: Record<number, number> = {};
+
+	i = 0;
+	while(i < arr1.length){
+		if(compareValues[arr1[i]] == undefined){
+			compareValues[arr1[i]] = 1;
+		}else{
+			compareValues[arr1[i]] = compareValues[arr1[i]] + 1;
+		}
+		i++;
+	}
+
+	i = 0;
+	while(i < arr2.length){
+		if(compareValues[arr2[i]] == undefined){
+			return false;
+		}
+		else if(compareValues[arr2[i]] == 1){
+			delete compareValues[arr2[i]];
+		}else{
+			compareValues[arr2[i]] = compareValues[arr2[i]] - 1;
+		}
+		i++;
+	}
+  return true;
+};
+
+//const word1 = 'acd', word2 = 'dca';
+//const word1 = "cabbbadd", word2 = "acbcbcdd";
+const word1 = "aaabbbbccddeeeeefffff", word2 = "aaaaabbcccdddeeeeffff";
+//const word1 = 'uau', word2 = 'ssx';
+console.log(closeStrings(word1, word2));
+
+/*
+solution 1 - no sorting
+function closeStrings(word1: string, word2: string): boolean {
+	if(word1.length !== word2.length) return false;
 	const rec1: Record<string, number> = {}, rec2: Record<string, number> = {};
 	let i = 0;
 	while(i < word1.length){
@@ -77,10 +143,5 @@ function closeStrings(word1: string, word2: string): boolean {
   return true;
 };
 
-//const word1 = 'acd', word2 = 'dca';
-//const word1 = "cabbbadd", word2 = "acbcbcdd";
-const word1 = "aaabbbbccddeeeeefffff", word2 = "aaaaabbcccdddeeeeffff";
-console.log(closeStrings(word1, word2));
 
-// musza byc te same litery
-// te same ilosci liter? np 3 a, 2 b i 1 c jest git z 3b, 2a i 1c?
+*/
