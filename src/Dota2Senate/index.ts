778d02c9d95120e01f86562cd1774e5e243849e4
murasame112 <tomaszwiesek00@gmail.com>
2025-11-15 17:07:13 +0100

    Dota2Senate solution 2

--- src/Dota2Senate/index.ts
+++ src/Dota2Senate/index.ts
@@ -1,4 +1,44 @@
 function predictPartyVictory(senate: string): string {
+	const radiant: number[] = [], dire: number[] = [];
+	for(let i = 0; i < senate.length; i++){
+		if(senate[i] === 'R') radiant.push(i);
+		else dire.push(i); 
+	}
+	
+	while(radiant.length > 0 && dire.length > 0){
+		if(radiant[0] < dire[0]){
+			radiant.push(radiant.shift()! + senate.length);
+			dire.shift();
+		}else{
+			dire.push(dire.shift()! + senate.length);
+			radiant.shift();
+		}
+	}
+
+	if(radiant.length > dire.length){
+		return 'Radiant';
+	}else{
+		return 'Dire';
+	}
+};
+
+//2 tablice z kolejnosciaqmi wystapien
+// R: 0 3 5 6 
+// D: 1 2 4 7
+
+//const senate = "DDRRR";
+const senate = "RDDRDRRD";
+//const senate = "RDD";
+//const senate = "DDDRRRRR" // expected: Radiant
+
+console.log(predictPartyVictory(senate));
+
+
+
+/*
+=== solution 1
+
+function predictPartyVictory(senate: string): string {
 	let rCount = 0, dCount = 0;
 	const queue = [...senate];
 	let checker = 0, deleter = 1;
@@ -29,8 +69,4 @@ function predictPartyVictory(senate: string): string {
 
 };
 
-//const senate = "DDRRR";
-//const senate = "RDDRDRRD";
-//const senate = "RDD";
-const senate = "DDDRRRRR" // expected: Radiant
-console.log(predictPartyVictory(senate));
+*/
