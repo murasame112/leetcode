5b54743ab22e5443f1959bfb31591aa591281108
murasame112 <tomaszwiesek00@gmail.com>
2025-12-02 15:05:37 +0100

    MaxTwinSumofLinedList solution1

--- src/MaxTwinSumofLinkedList/index.ts
+++ src/MaxTwinSumofLinkedList/index.ts
@@ -8,14 +8,62 @@ class ListNode {
 }
 
 function pairSum(head: ListNode | null): number {
-    return 0;
+	if(head === null) return 0;
+	let max: number = 0;
+	let n = 0;
+	const stack: number[] = [];
+	let current: ListNode | null = head;
+	while(current != null){
+		n++;
+		stack.push(current.val);
+		current = current.next;
+	}
+	current = head;
+	for(let i = 0; i < (n/2); i++){
+		let topVal = stack.pop();
+		let sum = current!.val + topVal!;
+		if(sum > max){
+			max = sum;
+		}
+		current = current!.next;
+	}
+
+  return max;
 };
 
-const l7 = new ListNode(7, null);
-const l6 = new ListNode(6, l7);
+
+const l6 = new ListNode(6, null);
 const l5 = new ListNode(5, l6);
 const l4 = new ListNode(4, l5);
 const l3 = new ListNode(3, l4);
 const l2 = new ListNode(2, l3);
 const l1 = new ListNode(1, l2);
 console.log(pairSum(l1));
+
+
+// ======== Solution 1 - with extra array
+/*
+function pairSum(head: ListNode | null): number {
+	if(head === null) return 0;
+	let max: number = 0;
+	let n = 0;
+	const stack: number[] = [];
+	let current: ListNode | null = head;
+	while(current != null){
+		n++;
+		stack.push(current.val);
+		current = current.next;
+	}
+	current = head;
+	for(let i = 0; i < (n/2); i++){
+		let topVal = stack.pop();
+		let sum = current!.val + topVal!;
+		if(sum > max){
+			max = sum;
+		}
+		current = current!.next;
+	}
+
+  return max;
+};
+*/
