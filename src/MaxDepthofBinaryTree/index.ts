3c51ff33a3a32f1fbfe60534d92d5d30cae1efce
murasame112 <tomaszwiesek00@gmail.com>
2025-12-02 16:48:10 +0100

    max depth done

--- src/MaxDepthofBinaryTree/index.ts
+++ src/MaxDepthofBinaryTree/index.ts
@@ -12,8 +12,19 @@
  
 
 function maxDepth(root: TreeNode | null): number {
-	return 0;
-    
+	if(root == null){
+		return 0;
+	}else{
+		let leftMax = maxDepth(root.left);
+		let rightMax = maxDepth(root.right);
+		return 1 + Math.max(leftMax, rightMax); 
+	}
 };
 
-//console.log(maxDepth());
+const t4 = new TreeNode(7, null, null);
+const t3 = new TreeNode(15, null, null);
+const t2 = new TreeNode(20, t3, t4);
+const t1 = new TreeNode(9, null, null);
+const t0 = new TreeNode(3, t1, t2);
+
+console.log(maxDepth(t0));
