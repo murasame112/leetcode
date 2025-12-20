c2a289bda4a4f6c42f26069f1f839a52e57c3c11
murasame112 <tomaszwiesek00@gmail.com>
2025-12-20 17:26:57 +0100

    path sum III solution 1

--- src/PathSumIII/index.ts
+++ src/PathSumIII/index.ts
@@ -9,25 +9,6 @@ class TreeNode {
     }
 }
 
-/*
-dla każdego poziomu zapamiętywać wszystkie sumy w hash map, np:
-0: [3]
-1: [8, 5]
-2: [14, 11, 6]
-cofamy w górę o 1 poziom, więc dla kazdego z 2: odemujemy 6 i dodajemy 2 (tak naprawdę zerujemy poziom 2 i dodajemy do niego na nowo 2)
-2: [10, 7, 2]
-3: [17, 14, 9, 7]
-cofamy w górę o 1 poziom, więc dla każdego z 3: odejmujemy 7 i dodajemy 4 (tak naprawdę zerujemy poziom 3 i dodajemy do niego na nowo 4)
-3: [14, 11, 6, 4]
-cofamy w górę o 3 poziomy, więc zerujemy wszystko oprócz 0: i potem dodajemy 1
-1: [4, 1]
-2: [13, 10, 9]
-cofamy w górę o 1 poziom, więc zerujemy poziom 2 i dodajemy do niego 8
-2: [12, 9, 8]
-rezultat - 14 wystąpiło 3 razy - linia 16, 19 i 21
-
-*/
-
 type LeveledNode = {
 	node: TreeNode;
 	level: number;
@@ -36,18 +17,19 @@ function pathSum(root: TreeNode | null, targetSum: number): number {
 	if(root == null) return 0;
 
 	const sums = new Map<number, number[]>();
-	
 
 	let counter: number = 0;
 	let stack: LeveledNode[] = [];
-	let visited: LeveledNode[] = [];
 	stack.push({node: root, level: 0});
 
 	while(stack.length > 0){
 		let current = stack.pop();
-		visited.push(current!);
-		console.log(`current level: ${current!.level}, current: ${current!.node.val}`);
 		let level = current!.level;
+		let value = current!.node.val;
+		
+		let updateCounter = updateSums(sums, level, value, targetSum);
+		counter += updateCounter;
+
 		if(current!.node.right != null) stack.push({node: current!.node.right, level: level + 1})
 		if(current!.node.left != null) stack.push({node: current!.node.left, level: level + 1})
 
@@ -55,6 +37,29 @@ function pathSum(root: TreeNode | null, targetSum: number): number {
 	return counter;
     
 };
+
+function updateSums(sums: Map<number, number[]>, level: number, value: number, target: number): number{
+	let counter = 0;
+	if(value == target) counter++;
+
+	if(level == 0){
+		sums.set(level, [value]);
+		return counter;
+	}
+	let valsOrig = sums.get(level - 1);
+	let vals = [...valsOrig!];
+	if(vals){
+		for(let i = 0; i < vals.length; i++){
+			let element = vals[i];
+			element += value;
+			if(element == target) counter++;
+			vals[i] = element;
+		}
+		vals.push(value);
+		sums.set(level, vals);
+	}
+	return counter;
+}
 
 const t8 = new TreeNode(4, null, null);
 const t7 = new TreeNode(7, null, null);
@@ -65,7 +70,18 @@ const t3 = new TreeNode(6, null, null);
 const t2 = new TreeNode(1, t5, t6);
 const t1 = new TreeNode(5, t3, t4);
 const t0 = new TreeNode(3, t1, t2);
+//const targetSum = 14;
 
 
-const targetSum = 14;
-console.log(pathSum(t0, targetSum));
+const s8 = new TreeNode(1, null, null);
+const s7 = new TreeNode(-2, null, null);
+const s6 = new TreeNode(3, null, null);
+const s5 = new TreeNode(11, null, null);
+const s4 = new TreeNode(2, null, s8);
+const s3 = new TreeNode(3, s6, s7);
+const s2 = new TreeNode(-3, null, s5);
+const s1 = new TreeNode(5, s3, s4);
+const s0 = new TreeNode(10, s1, s2);
+const targetSum = 8;
+
+console.log(pathSum(s0, targetSum));
